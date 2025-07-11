3d9cc8c0d24837f125df9f793fe2dfc1079e58c0
Juffry-Najah <najahproject48@gmail.com>
2025-07-11 10:44:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-import random
-characters = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-pw_length = int(input("Masukkin panjang kata sandi yang antum mau :) :"))
-generated_password = ""
+import discord
+from discord.ext import commands
+from logic import gen_pass
 
-for i in range(pw_length) :
-    generated_password += random.choice(characters)
+intents = discord.Intents.default()
+intents.message_content = True
 
-print("Kata sandi yang tergenerated :")
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def gen(ctx, pass_length = 10):
+    await ctx.send(gen_pass(pass_length))
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("token")
